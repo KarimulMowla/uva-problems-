472554599759e7da9db197ea20909a20977ee2c6
Mohammed Karimul Mowla <155143540+KarimulMowla@users.noreply.github.com>
2024-01-24 21:14:26 +0600

    Add files via upload

---
+++ 4. uva p11172.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    long int t,a,b,i;
+    scanf("%ld",&t);
+    for(i=1; i<=t; i++)
+    {
+        scanf("%ld%ld",&a,&b);
+        if(a<b)
+            printf("<\n");
+        else if(a>b)
+            printf(">\n");
+        else
+            printf("=\n");
+    }
+    return 0;
+}
